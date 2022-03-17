02418a868d83778e1a41b2a0e5fedbed99aa0c93
Tatiana <tatiana.store@orange.fr>
2022-03-17 09:46:18 +0100

    commencement du travail sur le front

---
+++ Téléchargements/202109-laloupe-p3-mag_centre-front-main(1)/202109-laloupe-p3-mag_centre-front-main/src/components/Article equitation/titre.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./titre.css";
+
+const titre = () => {
+   return (
+       <div className="main-titre">
+           <div className="exemple">
+               
+           </div>
+       </div>
+        
+    )
+}
